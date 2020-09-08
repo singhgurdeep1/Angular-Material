a9e767222002f8071c7d44fecd9dc101f0160712
Gurdeep Singh <gurdeep.singh@quovantis.com>
2020-09-08 09:53:23 +0530

    angular material theming setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClientCardComponent } from './client-card/client-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatCardModule,
+    MatGridListModule,
+    MatRadioModule,
+    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
