dd96c15240b8c6c247100e7746b327580baca0d9
singhgurdeep1 <gurdeep.singh@quovantis.com>
2020-09-17 11:44:03 +0530

    Passed data to table component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, Renderer2 } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
+import { PeriodicElement } from "./periodicModel";
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,27 @@ import { DOCUMENT } from '@angular/common';
 })
 export class AppComponent {
   title = 'demo-project';
+
+  PeriodicElements: PeriodicElement[] = [
+    { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+    { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+    { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+    { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+    { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+    { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+    { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+    { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+    { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+    { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+  ];
+
+  columns = [
+    { columnDef: 'position', header: 'Serial Number', cell: (element: any) => `${element.position}` },
+    { columnDef: 'name', header: 'Name', cell: (element: any) => `${element.name}` },
+    { columnDef: 'weight', header: 'Weight(g)', cell: (element: any) => `${element.weight}` },
+    { columnDef: 'symbol', header: 'Symbol', cell: (element: any) => `${element.symbol}` }
+  ];
+
 
   constructor(
     @Inject(DOCUMENT) private document: Document,
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,56 +1,42 @@
-import { Component, AfterViewInit, ViewChild } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, Input, OnInit } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-
-export interface PeriodicElement {
-  position: number;
-  name: string;
-  weight: number;
-  symbol: string;
-}
-
+import { PeriodicElement } from "../periodicModel";
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
-export class TableComponent implements AfterViewInit {
+export class TableComponent implements OnInit, AfterViewInit {
 
-  ELEMENT_DATA: PeriodicElement[] = [
-    { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-    { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-    { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-    { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-    { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-    { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-    { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-    { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-    { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-    { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-  ];
+  @Input() filter: boolean = false;
+  @Input() paging: boolean = false;
+  @Input() data: any[];
+  @Input() columns: any[];
 
-  columns = [
-    { columnDef: 'position', header: 'Serial Number', cell: (element: any) => `${element.position}` },
-    { columnDef: 'name', header: 'Name', cell: (element: any) => `${element.name}` },
-    { columnDef: 'weight', header: 'Weight(g)', cell: (element: any) => `${element.weight}` },
-    { columnDef: 'symbol', header: 'Symbol', cell: (element: any) => `${element.symbol}` }
-  ];
-
-  displayedColumns: string[] = this.columns.map(result => result.columnDef);
-  dataSource = new MatTableDataSource<PeriodicElement>(this.ELEMENT_DATA);
+  dataSource: any;
+  displayedColumns: string[];
 
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor() { }
 
-  ngAfterViewInit() {
-    console.log(this.dataSource);
+  ngOnInit(): void {
+    this.displayedColumns = this.columns.map(result => result.columnDef);
+    this.dataSource = new MatTableDataSource<any>(this.data);
+  }
 
+  ngAfterViewInit() {
     this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
